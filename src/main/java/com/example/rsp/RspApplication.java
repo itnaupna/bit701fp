c634ce6398d0bd1eb4896aa387d48feb4620dac5
cyj <itnaupna@gmail.com>
2023-06-21 16:07:30 +0900

    init commit

---
+++ src/main/java/com/example/rsp/RspApplication.java
@@ -0,0 +1,14 @@
+package com.example.rsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RspApplication {
+
+	//eong
+	public static void main(String[] args) {
+		SpringApplication.run(RspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rsp/RspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
